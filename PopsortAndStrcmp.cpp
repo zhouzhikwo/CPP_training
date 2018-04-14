1becf2281b00ff0e6e212eac2610d640cac1ddc0
zhouzhikwo <zhouzhikwo@outlook.com>
2018-04-14 20:08:21 +0800

    Third Update

--- PopsortAndStrcmp.cpp
+++ PopsortAndStrcmp.cpp
@@ -13,21 +13,20 @@ int mystrcmp(const char* p1, const char* p2) {
 	return *p1 - *p2;
 }
 
-void mypopsort(int *pList, int listSize) {
+template <class T>
+void MyPopsort(T *pList, int listSize) {
 	int temp;
 	for (int i = 0; i < listSize; i++) {
-		for (int j = 0; j < listSize; j++) {
-			if (pList[i] < pList[j]) {
+		for (int j = i + 1; j < listSize; j++) {
+			if (pList[i] > pList[j]) {
 				temp = pList[i];
 				pList[i] = pList[j];
 				pList[j] = temp;
 			}
 		}
 	}
-
 	return;
 }
-
 void main(void) {
 	const char* p1 = "stringA";
 	const char* p2 = "string";
@@ -35,7 +34,15 @@ void main(void) {
 
 	int pList[] = { 1,6,8,9,7,4 };
 	int listSize = 6;
-	mypopsort(pList, listSize);
+	MyPopsort(pList, listSize);
+	for (int i = 0; i < listSize; i++) {
+		cout << pList[i] << ' ';
+	}
+	cout << endl;
+
+	double pList1[] = { 1.1,6.1,8.1,9.1,7.1,4.1 };
+	int listSize1 = 6;
+	MyPopsort(pList1, listSize1);
 	for (int i = 0; i < listSize; i++) {
 		cout << pList[i] << ' ';
 	}
